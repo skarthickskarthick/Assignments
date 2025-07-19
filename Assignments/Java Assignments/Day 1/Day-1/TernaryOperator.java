5db053a785660c96144eb9c449537295a21b65aa
skarthickskarthick <139897562+skarthickskarthick@users.noreply.github.com>
2025-07-19 12:56:34 +0530

    Add files via upload

---
+++ Assignments/Java Assignments/Day 1/Day-1/TernaryOperator.java
@@ -0,0 +1,10 @@
+package org.example.Day_1;
+
+public class TernaryOperator {
+        public static void main(String args[])
+        {
+            int a=10;
+            int b=12;
+            System.out.println(a>b?a:b);
+        }
+}
